b675f0ca3b94ebbdad3eb7a659d19776037a144f
Niranjan <niranjan0881@gmail.com>
2022-11-21 13:33:49 +0530

    latest

--- app.ts
+++ app.ts
@@ -81,4 +81,80 @@ function anotherFunChallenge(input) {
 // Different terms for inputs
 // Drop Non Dominants
 
-//Worst Case  
+//Worst Case
+// always think about the worst case like when looping through the array to find nemo
+// except it to be at the last position too
+
+//Drop the constants
+// const printFirstItemThenFirstHalfThenSayHiTenTimes = (array: Array<number>) => {
+//   console.log(array[0]);
+
+//   const middleIndex = Math.floor(array.length / 2);
+//   let index = 0;
+
+//   while (index < middleIndex) {
+//     console.log(array[index]);
+//     index++;
+//   }
+
+//   for (let index = 0; index < 10; index++) {
+//     console.log("Hi");
+//   }
+// };
+
+// printFirstItemThenFirstHalfThenSayHiTenTimes([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+// //BIG 0  = O(1) + 0(n/2) + O(100)  = o(n)
+
+// //another example of drop the constants
+// const compressBoxesTwice = (boxes) => {
+//   boxes.forEach((box) => console.log(box));
+//   boxes.forEach((box) => console.log(box));
+// };
+
+// // O(2n)  = O(n)
+
+//O(n^2) quadratic time
+// const logPairs = (arr: number[]) => {
+//   arr.forEach((num1) => arr.forEach((num2) => console.log(num1, num2)));
+// };
+
+// logPairs([1, 2, 3, 4, 5]);
+// //O(n^2)
+
+// //different terms have different inputs
+// const compressBoxesTwice = (boxes1, boxes2) => {
+//   boxes1.forEach((box) => console.log(box));
+//   boxes2.forEach((box) => console.log(box));
+// };
+//now the big O  is O( a + b) where the inputs are different
+// if the loops were nested the big O would be O(a * b)
+
+// const printAllNumbersThenAllPairSums = (arr: number[]) => {
+//   arr.forEach((num) => console.log(num));
+
+//   console.log("and their sums are : ");
+
+//   arr.forEach((a) => arr.forEach((b) => console.log(a + b)));
+// };
+
+// printAllNumbersThenAllPairSums([1, 2]);
+//BIG O  =  O(n) + O(n^2)
+//here we apply the rule number 4  and take only the non dominant terms
+
+//SPACE COMPLEXITY
+const logBooo = (n: number) => {
+  for (let i = 0; i < n; i++) {
+    console.log("booo");
+  }
+};
+//the space complexity is O(1) since there is only one memory allocation ie the i variable
+
+const arrayOfHiNTimes = (n: number) => {
+  const hiArray = [];
+  for (let i = 0; i < n; i++) {
+    hiArray[i] = "hi";
+  }
+  return hiArray;
+};
+//the space complexity is O(n) since there is  memory allocation of a data structure
